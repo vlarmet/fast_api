c2c890d026e24e2ec2e43876c0777a691eba444b
vlarmet <vincent.larmet@apeiron.technology>
2023-11-24 17:45:18 +0100

    try again

--- tests/test.py
+++ tests/test.py
@@ -6,7 +6,7 @@ import json
 
 def test_proba():
        
-    with open('client1.json') as data_file:
+    with open('tests/client1.json') as data_file:
         myjson = json.load(data_file)
     myjson = json.dumps(myjson)
     r = requests.get("http://127.0.0.1:8000/prediction/", data = myjson)
