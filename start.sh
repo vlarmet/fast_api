4c85f360e3f9967fb39ef973fd5fef1f15354471
vlarmet <vincent.larmet@apeiron.technology>
2023-11-24 14:34:19 +0100

    remove sudo

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Mise à jour des paquets
-sudo apt-get update
+apt-get update
 
 # Installation des dépendances
-sudo apt-get -y install curl
-sudo apt-get install libgomp1
+apt-get -y install curl
+apt-get install libgomp1
 
 gunicorn -w 2 -k uvicorn.workers.UvicornWorker main:app
