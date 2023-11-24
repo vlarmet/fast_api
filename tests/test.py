d2bf28e73d80ee129211388407399bd8133b2ae4
vlarmet <vincent.larmet@apeiron.technology>
2023-11-24 18:46:08 +0100

    2 clients unit tests

--- tests/test.py
+++ tests/test.py
@@ -1,14 +1,22 @@
 import pytest
-import requests
 import json
-
+from multiprocessing import Process
+from ..main import get_prediction
 
 
 def test_proba():
-       
+    
     with open('tests/client1.json') as data_file:
         myjson = json.load(data_file)
-    myjson = json.dumps(myjson)
-    r = requests.get("http://127.0.0.1:8000/prediction/", data = myjson)
-    r = json.loads(r.text)
-    assert round(r["probability"] , 2) == 0.07
+    prob = get_prediction(myjson)
+        
+    assert round(prob["probability"] , 2) == 0.07
+        
+
+def test_proba_2():
+       
+    with open('tests/client2.json') as data_file:
+        myjson = json.load(data_file)
+    prob = get_prediction(myjson)
+        
+    assert round(prob["probability"] , 2) == 0.46
