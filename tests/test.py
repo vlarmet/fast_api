fd58d83328165c7e0ce9e9e9d0593372840e8dd2
vlarmet <vincent.larmet@apeiron.technology>
2023-11-24 17:28:06 +0100

    unit test

---
+++ tests/test.py
@@ -0,0 +1,14 @@
+import pytest
+import requests
+import json
+
+
+
+def test_proba():
+       
+    with open('client1.json') as data_file:
+        myjson = json.load(data_file)
+    myjson = json.dumps(myjson)
+    r = requests.get("http://127.0.0.1:8000/prediction/", data = myjson)
+    r = json.loads(r.text)
+    assert round(r["probability"] , 2) == 0.07
