a21c3f677bac9634f7223b34c115d5e6a5b595c2
vlarmet <vincent.larmet@apeiron.technology>
2023-11-28 09:17:40 +0100

    json as str

--- tests/test_api_get.py
+++ tests/test_api_get.py
@@ -7,8 +7,9 @@ import requests
 def test_proba():
     
     with open('tests/client1.json') as data_file:
-        myjson = json.loads(data_file)
-    prob = requests.get(url = 'http://127.0.0.1:8000/prediction/', data = myjson)
+        myjson = json.load(data_file)
+        myjson = json.dump(myjson)
+    prob = requests.get(url = 'http://127.0.0.1:8000/prediction/', data = myjson).text
         
     assert round(prob["probability"] , 2) == 0.07
         
@@ -16,8 +17,9 @@ def test_proba():
 def test_proba_2():
        
     with open('tests/client2.json') as data_file:
-        myjson = json.loads(data_file)
-    prob = requests.get(url = 'http://127.0.0.1:8000/prediction/', data = myjson)
+        myjson = json.load(data_file)
+        myjson = json.dump(myjson)
+    prob = requests.get(url = 'http://127.0.0.1:8000/prediction/', data = myjson).text
         
     assert round(prob["probability"] , 2) == 0.46
 
@@ -26,7 +28,7 @@ def test_proba3():
     
     
     myjson = '{"index" : 73}'
-    prob = requests.get(url = 'http://127.0.0.1:8000/prediction2/', data = myjson)
+    prob = requests.get(url = 'http://127.0.0.1:8000/prediction2/', data = myjson).text
         
     assert round(prob["probability"] , 2) == 0.48
         
