a4df8665382b349bc27bfbef1ecd3503ea431211
vlarmet <vincent.larmet@apeiron.technology>
2023-11-28 09:21:57 +0100

    dumps instead of dump

--- tests/test_api_get.py
+++ tests/test_api_get.py
@@ -8,9 +8,9 @@ def test_proba():
     
     with open('tests/client1.json') as data_file:
         myjson = json.load(data_file)
-        myjson = json.dump(myjson)
+        myjson = json.dumps(myjson)
     prob = requests.get(url = 'http://127.0.0.1:8000/prediction/', data = myjson).text
-        
+    prob = json.loads(prob)
     assert round(prob["probability"] , 2) == 0.07
         
 
@@ -18,9 +18,9 @@ def test_proba_2():
        
     with open('tests/client2.json') as data_file:
         myjson = json.load(data_file)
-        myjson = json.dump(myjson)
+        myjson = json.dumps(myjson)
     prob = requests.get(url = 'http://127.0.0.1:8000/prediction/', data = myjson).text
-        
+    prob = json.loads(prob)
     assert round(prob["probability"] , 2) == 0.46
 
 # test envoi du json client
@@ -29,6 +29,7 @@ def test_proba3():
     
     myjson = '{"index" : 73}'
     prob = requests.get(url = 'http://127.0.0.1:8000/prediction2/', data = myjson).text
+    prob = json.loads(prob)
         
     assert round(prob["probability"] , 2) == 0.48
         
